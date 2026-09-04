efc6695328cbe9ef7b56a9fd7236033bba056a74
IwamotoKoki <c25002@std.it-college.ac.jp>
2026-09-04 20:23:17 +0900

    refactor: PlayerControllerの入力処理をPlayerInputReaderへ移行

--- Assets/RobotAction/Scripts/GamePlay/PlayerController.cs
+++ Assets/RobotAction/Scripts/GamePlay/PlayerController.cs
@@ -1,35 +1,57 @@
 using UnityEngine;
 
-namespace RobotAction.GamePlay.Player
+namespace RobotAction.Gameplay.Player
 {
     public class PlayerController : MonoBehaviour
     {
+        private const float BoostDeadZoneSqr = 0.01f;
+
+        private PlayerInputReader _inputReader;
+
         [SerializeField] private Rigidbody _rigidbody;
         [SerializeField] private float _moveSpeed;
         [SerializeField] private float _boostSpeed;
 
-        private void Update()
+        private void Awake()
         {
-            float axisX = Input.GetAxisRaw("Horizontal");//TODO:InputSystem‚É‘Î‰ž‚³‚¹‚é
-            float axisZ = Input.GetAxisRaw("Vertical");//TODO:InputSystem‚É‘Î‰ž‚³‚¹‚é
+            _inputReader = new PlayerInputReader(new PlayerInputActions());
+        }
 
-            if (axisX != 0)
+        private void OnEnable()
+        {
+            _inputReader.OnBoost += Boost;
+        }
+
+        private void FixedUpdate()
+        {
+            Vector2 input = _inputReader.MoveDirection;
+
+            if (input.x != 0)
             {
-                _rigidbody.AddForce(axisX * _moveSpeed * transform.right,
+                _rigidbody.AddForce(input.x * _moveSpeed * transform.right,
                                     ForceMode.Force);
             }
 
-            if(axisZ != 0)
+            if (input.y != 0)
             {
-                _rigidbody.AddForce(axisZ * _moveSpeed * transform.forward,
+                _rigidbody.AddForce(input.y * _moveSpeed * transform.forward,
                                     ForceMode.Force);
             }
+        }
 
-            if(Input.GetKeyDown(KeyCode.LeftShift))//TODO:InputSystem‚É‘Î‰ž‚³‚¹‚é
-            {
-                _rigidbody.AddForce(_boostSpeed * new Vector3(axisX, 0, axisZ),
-                                    ForceMode.Impulse);
-            }
+        private void OnDisable()
+        {
+            _inputReader.OnBoost -= Boost;
+            _inputReader.Dispose();
+        }
+
+        private void Boost()
+        {
+            Vector2 input = _inputReader.MoveDirection;
+            input = input.sqrMagnitude > BoostDeadZoneSqr ? input.normalized : input;
+
+            _rigidbody.AddForce(_boostSpeed * new Vector3(input.x, 0, input.y),
+                                ForceMode.Impulse);
         }
     }
 }

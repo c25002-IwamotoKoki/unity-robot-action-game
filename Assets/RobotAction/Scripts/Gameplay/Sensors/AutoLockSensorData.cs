a4572274722dbbbf8e6629d5b02cb2acce565a0f
IwamotoKoki <c25002@std.it-college.ac.jp>
2026-09-13 19:37:36 +0900

    feat(sensor): TargetBufferを追加
    Sensorで取得した情報を格納するための純粋なC#クラス

---
+++ Assets/RobotAction/Scripts/Gameplay/Sensors/AutoLockSensorData.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace RobotAction.Gameplay.Sensor
+{
+    [CreateAssetMenu(fileName = "AutoLockSensorData", menuName = "Scriptable Objects/AutoLockSensorData")]
+    public class AutoLockSensorData : ScriptableObject
+    {
+        [SerializeField, Min(1)] private int _maxSearch;
+        [SerializeField] private float _searchRange;
+        [SerializeField] private Vector3 _searchPositionOffset;
+        [SerializeField] private float _searchCoolTime;
+
+        public int MaxSearch => _maxSearch;
+        public float SearchRange => _searchRange;
+        public Vector3 SearchPositionOffset => _searchPositionOffset;
+        public float SearchCoolTime => _searchCoolTime;
+    }
+}

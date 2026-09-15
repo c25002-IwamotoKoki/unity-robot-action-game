ada33e54f772640b473c0601caf2e464205a8b1e
IwamotoKoki <c25002@std.it-college.ac.jp>
2026-09-15 17:38:02 +0900

    feat(gun): 銃のSOデータの抽象基底クラスGunDataを追加
    銃の基底クラスでどの銃か関係なくつかめるようにするために追加

--- Assets/RobotAction/Scripts/Gameplay/Scriptables/KineticGunData.cs
+++ Assets/RobotAction/Scripts/Gameplay/Scriptables/KineticGunData.cs
@@ -3,15 +3,13 @@ using UnityEngine;
 namespace RobotAction.Gameplay.Scriptables
 {
     [CreateAssetMenu(fileName = "KineticGunData", menuName = "Scriptable Objects/KineticGunData")]
-    public class KineticGunData : ScriptableObject
+    public class KineticGunData : GunData
     {
-        [SerializeField] private string _name;
         [SerializeField] private float _fireRate;
         [SerializeField] private float _bulletLifeTime;
         [SerializeField] private float _bulletSpeed;
         [SerializeField] private float _bulletBaseAttackPower;
 
-        public string Name => _name;
         public float FireRate => _fireRate;
         public float BulletLifeTime => _bulletLifeTime;
         public float BulletSpeed => _bulletSpeed;

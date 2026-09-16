19463d1d01a278d5d71aaa7ecdfbbd1aca83ea65
IwamotoKoki <c25002@std.it-college.ac.jp>
2026-09-16 12:27:48 +0900

    feat(weapon): WeaponPartsHandlerを追加
    付近の武器の索敵と一番近い武器を取得するメソッドなどを内包

---
+++ Assets/RobotAction/Scripts/Gameplay/Parts/Weapons/WeaponPartsHandler.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace RobotAction.Gameplay.Parts.Weapons
+{
+    public class WeaponPartsHandler : PartsHandlerBase<IWeaponPart>
+    {
+        [SerializeField] private float _weaponSarchRange;
+        [SerializeField,Min(1)] private int _maxWeaponSarch;
+        [SerializeField] private LayerMask _sarchLayer;
+
+        private Collider[] _detectedColliders;
+        private List<IWeaponPart> _detectedWeaponParts;
+
+        private void Awake()
+        {
+            _detectedColliders = new Collider[_maxWeaponSarch];
+            _detectedWeaponParts = new(_maxWeaponSarch);
+        }
+
+        public bool TryPickUpNearlyWeapon()
+        {
+            if(TrySearchWeapon())
+            {
+                Equip(_detectedWeaponParts[0]);
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool TrySearchWeapon()
+        {
+            _detectedWeaponParts.Clear();
+
+            int searchCount = Physics.OverlapSphereNonAlloc(transform.position,
+                                                           _weaponSarchRange,
+                                                           _detectedColliders,
+                                                           _sarchLayer);
+
+            if(searchCount != 0)
+            {
+                for(int i = 0; i < searchCount; i++)
+                {
+                    if(_detectedColliders[i].TryGetComponent(out IWeaponPart part))
+                    {
+                        if(!part.Owner)
+                        {
+                            _detectedWeaponParts.Add(part);
+                        }
+                    }
+                }
+            }
+
+            return _detectedWeaponParts.Count > 0;
+        }
+
+        public void Attack()
+        {
+            CurrentPart?.Attack();
+        }
+
+        public void Purge()
+        {
+            Unequip();
+        }
+    }
+}

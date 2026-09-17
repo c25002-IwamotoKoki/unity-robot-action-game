c2517b8b9d44ebae95117bcd18a2152d40acf177
IwamotoKoki <c25002@std.it-college.ac.jp>
2026-09-17 21:58:59 +0900

    feat(enemy): Stateで使う変数を格納す仲介役クラスBlackBoardを追加

---
+++ Assets/RobotAction/Scripts/Gameplay/Enemy/BlackBoard.cs
@@ -0,0 +1,8 @@
+namespace RobotAction.Gameplay.Enemy
+{
+    public class BlackBoard
+    {
+        public bool IsThinking { get; set; }
+        public float ThinkInterval { get; set; } = 0.5f;
+    }
+}

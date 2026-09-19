dacfee44cd74f5d687b5348cabddc76208b3d320
IwamotoKoki <c25002@std.it-college.ac.jp>
2026-09-20 08:49:58 +0900

    feat(enemy): PatrolState内にランダムな方向に移動する処理を追加

--- Assets/RobotAction/Scripts/Gameplay/Enemy/PatrolState.cs
+++ Assets/RobotAction/Scripts/Gameplay/Enemy/PatrolState.cs
@@ -1,7 +1,10 @@
+using UnityEngine;
+
 namespace RobotAction.Gameplay.Enemy
 {
     public class PatrolState : StateBase
     {
+        private Vector3 _moveDirection;
         public PatrolState(StateMachine ownerMachine,
                            EnemyBase ownerEnemy,
                            Blackboard blackboard) 
@@ -16,6 +19,15 @@ namespace RobotAction.Gameplay.Enemy
         public override void OnPeriodicTick()
         {
             _ownerEnemy.SearchNearlyTarget();
+
+            _moveDirection = _ownerEnemy.transform.position;
+
+            //ƒ‰ƒ“ƒ_ƒ€‚È•ûŒü‚Ö‚ÌˆÚ“®‚É—p‚¢‚é‚½‚ß-1ˆÈã2–¢–‚ÅŒˆ‚ß‚Ä‚¢‚Ü‚·
+            //‚±‚±‚Å-1~1‚Ì”ÍˆÍ‚É‚µ‚Ä‚¢‚é‚Ì‚ÍMoveSpeed‚Í_ownerEnemy“à‚Éˆê”C‚µ‚Ä‚¢‚é‚½‚ß
+            //ˆÚ“®•ûŒü‚¾‚¯‚ğƒ‰ƒ“ƒ_ƒ€‚ÅŒˆ’è‚µ‚½‚¢‚Æ‚¢‚¤ˆÓ}‚ª‚ ‚é‚©‚ç‚Å‚·
+            _moveDirection.x = Random.Range(-1, 2);
+            _moveDirection.y = 0;
+            _moveDirection.z = Random.Range(-1, 2);
         }
 
         public override void OnTick()
@@ -24,6 +36,11 @@ namespace RobotAction.Gameplay.Enemy
             {
                 _ownerMachine.ChangeState(_ownerEnemy.ChaseState);
             }
+
+            Debug.Log($"{_moveDirection}‚ÖˆÚ“®’†...");
+
+            _ownerEnemy.RotateTowards(_moveDirection);
+            _ownerEnemy.Move(_moveDirection);
         }
     }
 }

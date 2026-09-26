1af269b79f282593246e10013aaade3b833a2b8b
IwamotoKoki <c25002@std.it-college.ac.jp>
2026-09-26 13:44:39 +0900

    fix(gun): 抽象基底ScriptableがAssetsMenuに表示されていた問題を修正

--- Assets/RobotAction/Scripts/Gameplay/Parts/Weapons/Guns/Base/GunData.cs
+++ Assets/RobotAction/Scripts/Gameplay/Parts/Weapons/Guns/Base/GunData.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 namespace RobotAction.Gameplay.Parts.Weapons.Guns
 {
-    [CreateAssetMenu(fileName = "GunData", menuName = "Scriptable Objects/GunData")]
     public abstract class GunData : ScriptableObject
     {
         [SerializeField] private string _name;

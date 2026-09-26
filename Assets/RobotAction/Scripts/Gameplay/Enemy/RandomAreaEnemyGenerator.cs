0b590e39eb5443bd02f8b2825a42632ca615bade
IwamotoKoki <c25002@std.it-college.ac.jp>
2026-09-26 21:05:20 +0900

    feat(enemy): ランダムな地点に敵を生成する機能を実装しそれに伴いDefaultEnemyPrefabを追加

---
+++ Assets/RobotAction/Scripts/Gameplay/Enemy/RandomAreaEnemyGenerator.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+namespace RobotAction.Gameplay.Enemy
+{
+    public class RandomAreaEnemyGenerator : MonoBehaviour
+    {
+        [SerializeField] private EnemyPool _enemyPool;
+        [SerializeField] private Transform[] _spawnPoints;
+        [SerializeField] private float _spawnRate;
+        [SerializeField] private int _capacity;
+
+        private float _spawnTimer;
+        private int _lastSpawnIndex = -1;
+
+
+        private void Awake()
+        {
+            TryGetComponent(out _enemyPool);
+            _enemyPool.SetCapacity(_capacity);
+        }
+
+        private void Start()
+        {
+            _enemyPool.PrewarmPool();
+        }
+
+        private void Update()
+        {
+            _spawnTimer += Time.deltaTime;
+
+            if(_spawnTimer >= _spawnRate)
+            {
+                _spawnTimer = 0;
+                SpawnEnemy();
+            }
+        }
+
+        public void SpawnEnemy()
+        {
+            if (!_enemyPool.CanSpawn) return;
+
+            EnemyBase enemy = _enemyPool.Spawn();
+
+            int spawnIndex = GetSpawnIndex();
+            Transform spawnPoint = _spawnPoints[spawnIndex];
+
+            enemy.transform.SetPositionAndRotation(spawnPoint.position,spawnPoint.rotation);
+        }
+
+        private int GetSpawnIndex()
+        {
+            if(_spawnPoints.Length <= 1)
+            {
+                return 0;
+            }
+
+            //重複防止処理
+            int index;
+            do
+            {
+                index = Random.Range(0,_spawnPoints.Length);
+
+            } while(index == _lastSpawnIndex);
+
+            _lastSpawnIndex = index;
+
+            return index;
+        }
+
+    }
+}
